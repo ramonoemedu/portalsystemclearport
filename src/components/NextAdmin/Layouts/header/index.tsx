d0c04f77a88cb3d37503224dacff309bbbb7bc89
Ramon Oem <ramonoem@Ramons-MacBook-Pro-M4.local>
2026-02-15 11:34:45 +0700

    [TASK] remove notification

--- src/components/NextAdmin/Layouts/header/index.tsx
+++ src/components/NextAdmin/Layouts/header/index.tsx
@@ -66,7 +66,7 @@ export function Header() {
 
         <ThemeToggleSwitch />
 
-        <Notification />
+        {/* <Notification /> */}
 
         <div className="shrink-0">
           <UserInfo />
